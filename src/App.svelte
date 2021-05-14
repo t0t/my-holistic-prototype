724a8aa734d596153fabe461e298645e05ebc6dc
iClon Z4 <iclon@iMac.local>
2021-05-14 12:08:20 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<main>
+	<h1>Hola, éste es mi prototipo holistico</h1>
+	<p>yeah!!!!</p>
+</main>
+
+<style>
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		background-color: pink;
+	}
+
+	h1 {
+		color: black;
+		text-transform: uppercase;
+		font-size: 6em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
